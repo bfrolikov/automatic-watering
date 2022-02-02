6106f2f58c2598aad11276910e5a47c404c30e23
bfrolikov <45406111+bfrolikov@users.noreply.github.com>
2022-02-03 01:51:46 +0300

    Code style fixes

--- AutomaticWatering.ino
+++ AutomaticWatering.ino
@@ -5,22 +5,20 @@ const int DETECTOR_THRESHOLD = 450;
 void addWater();
 
 void setup() {
-  pinMode(RELAY_PIN,OUTPUT);
-  digitalWrite(RELAY_PIN,HIGH);
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, HIGH);
   delay(9000);
 }
 
 void loop() {
-  if(analogRead(DETECTOR_PIN) >= DETECTOR_THRESHOLD)
-  {
-    //soil is dry
+  if(analogRead(DETECTOR_PIN) >= DETECTOR_THRESHOLD) {
      addWater();
   }
   delay(3000);
 }
 
-void addWater(){
-  digitalWrite(RELAY_PIN,LOW);
+void addWater() {
+  digitalWrite(RELAY_PIN, LOW);
   delay(1100);
-  digitalWrite(RELAY_PIN,HIGH);
+  digitalWrite(RELAY_PIN, HIGH);
 }
